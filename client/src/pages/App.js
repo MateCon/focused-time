a17ae6297c977b7ead23e899ace31500a24b81e0
MateCon <68882595+MateCon@users.noreply.github.com>
2021-06-30 12:55:16 -0300

    install things

---
+++ client/src/pages/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import '../styles/App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      Hello world!
+    </div>
+  );
+}
+
+export default App;
