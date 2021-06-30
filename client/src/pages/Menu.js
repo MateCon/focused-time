322b73976e68e61c2130bae42794dbd43be8c3e4
MateCon <68882595+MateCon@users.noreply.github.com>
2021-06-30 14:48:52 -0300

    style for the home

--- client/src/pages/Menu.js
+++ client/src/pages/Menu.js
@@ -3,16 +3,18 @@ import { ReactComponent as Logo } from '../images/logo.svg';
 
 const Menu = () => {
     return (
-        <div>
+        <div className='Menu'>
             <div id='background-text'>
-                <p>POMODORO TIMER</p>
-                <p>TIME MANAGEMENT</p>
+                <p class='first'>POMODORO TIMER</p>
+                <p class='second'>TIME MANAGEMENT</p>
             </div>
             <h1 id='title'>Focused Time</h1>
             <h2 id='subtitle'>By MateCon</h2>
-            <Logo class='logo'/>
+            <Logo id='logo'/>
             <p id='start-link'>Start!</p>
-            <p id='about-me'>About me</p>
+            <div id='about-me'>
+                <a href='https://github.com/matecon' target='blanc'>About me</a>
+            </div>
         </div>
     );
 }
