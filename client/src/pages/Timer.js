68bc6034dbfe789c3f214c2989716067f871c2d2
MateCon <68882595+MateCon@users.noreply.github.com>
2021-07-09 12:10:21 -0300

    timer elements

--- client/src/pages/Timer.js
+++ client/src/pages/Timer.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 const Timer = () => {
     return (
-        <p>Timer</p>
+        <div id='timer'>
+            <p id='time'>13:04</p>
+            <div id='pomodoro-options-container'>
+                <button id='pomodoro-selector'>Pomodoro</button>
+                <button id='break-selector'>Break</button>
+                <button id='long-break-selector'>Long break</button>
+            </div>
+            <div id='time-control-container'>
+                <button id='start-button'>Start</button>
+                <button id='pause-button'>Pause</button>
+                <button id='restart-button'>Restart</button>
+            </div>
+        </div>
     );
 }
 
