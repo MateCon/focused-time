512160e46587e25a66de3a1d9bc4475ad6763057
MateCon <68882595+MateCon@users.noreply.github.com>
2021-07-09 16:43:38 -0300

    css for navbar and timer

--- client/src/pages/Navbar.js
+++ client/src/pages/Navbar.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import { ReactComponent as Logo } from '../images/logo.svg';
+import { ReactComponent as Home } from '../images/home.svg';
+import { ReactComponent as Clock } from '../images/clock.svg';
+import { ReactComponent as Profile } from '../images/profile.svg';
 
 const Navbar = () => {
     return (
-        null
+        <div id='navbar'>
+            <div>
+                <Logo id='logo' />
+                <p id='title'>Focused Time</p>
+            </div>
+            <div>
+                <Home className='icon' />
+                <p>Home</p>
+            </div>
+            <div>
+                <Clock className='icon' />
+                <p>Clock</p>
+            </div>
+            <div>
+                <Profile className='icon' />
+                <p>Profile</p>
+            </div>
+        </div>
     );
 }
 
--- client/src/pages/Timer.js
+++ client/src/pages/Timer.js
@@ -1,18 +1,26 @@
 import React from 'react';
+import Navbar from './Navbar';
 
 const Timer = () => {
     return (
-        <div id='timer'>
-            <p id='time'>13:04</p>
-            <div id='pomodoro-options-container'>
-                <button id='pomodoro-selector'>Pomodoro</button>
-                <button id='break-selector'>Break</button>
-                <button id='long-break-selector'>Long break</button>
-            </div>
-            <div id='time-control-container'>
-                <button id='start-button'>Start</button>
-                <button id='pause-button'>Pause</button>
-                <button id='restart-button'>Restart</button>
+        <div>
+            <Navbar />
+            <div id='timer'>
+                <div id='pomodoro-options-container'>
+                    <button id='pomodoro-selector'>Pomodoro</button>
+                    <button id='break-selector'>Break</button>
+                    <button id='long-break-selector'>Long break</button>
+                </div>
+            
+                <div id='time-container' />
+                <div id='time-conteiner-compleated' />
+                <p id='time'>13:04</p>
+
+                <div id='time-control-container'>
+                    <button id='start-button'>Start</button>
+                    <button id='pause-button'>Pause</button>
+                    <button id='restart-button'>Restart</button>
+                </div>
             </div>
         </div>
     );
