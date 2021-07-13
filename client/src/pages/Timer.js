f6a0c3f8a60c4f9f7f0869447b0e3cef0d9cf009
MateCon <68882595+MateCon@users.noreply.github.com>
2021-07-13 08:39:31 -0300

    full-screen functionality and reset with r

--- client/src/pages/Timer.js
+++ client/src/pages/Timer.js
@@ -3,11 +3,13 @@ import useSound from 'use-sound';
 import Navbar from './Navbar';
 import { ReactComponent as ClockBorder } from '../images/clock-border.svg';
 import { ReactComponent as FullScreen } from '../images/full-screen.svg';
+import { ReactComponent as Close } from '../images/close.svg';
 import alarmKitchen from '../sounds/kitchen-alarm.mp3';
 
 
 const Timer = ({time, ratio, isRunning, start, pause, resume, restart, setPomodoro, setBreak, setLongBreak, goToMenu, goToTimer, goToProfile}) => {
     const [hasStarted, setHasStarted] = useState(false);
+    const [isFullscreenOn, setIsFullscreenOn] = useState(false);
     const [timerBackground, setTimerBackground] = useState('');
     const [playAlarm] = useSound(alarmKitchen);
 
@@ -31,36 +33,48 @@ const Timer = ({time, ratio, isRunning, start, pause, resume, restart, setPomodo
                     pause();
                 }
             }
+        } else if(event.key === 'r') {
+            if(hasStarted) {
+                restart();
+                setHasStarted(false);
+            }
         }
     }
 
     return (
-        <div onKeyPress={handleKeyPress} tabIndex={0} style={{ outline: 'none' }}>
+        <div onKeyPress={handleKeyPress} tabIndex={0} style={{ outline: 'none' }} className={isFullscreenOn ? 'full-screen' : ''} >
             <Navbar goToMenu={goToMenu} goToTimer={goToTimer} goToProfile={goToProfile} />
             <div id='top-right'>
-                <FullScreen className='icon' />
+                {
+                    isFullscreenOn 
+                        ? <Close className='icon' onClick={() => setIsFullscreenOn(false)} />
+                        : <FullScreen className='icon' onClick={() => setIsFullscreenOn(true)} />
+                }
             </div>
             <div id='timer'>
-                <div id='pomodoro-options-container'>
-                    <button id='pomodoro-selector' onClick={() => {
-                        setPomodoro();
-                        setHasStarted(false);
-                    }}>Pomodoro</button>
-                    <button id='break-selector' onClick={() => {
-                        setBreak();
-                        setHasStarted(false);
-                    }}>Break</button>
-                    <button id='long-break-selector' onClick={() => {
-                        setLongBreak();
-                        setHasStarted(false);
-                    }}>Long break</button>
-                </div>
+                {
+                    !isFullscreenOn ? <div id='pomodoro-options-container'>
+                        <button id='pomodoro-selector' onClick={() => {
+                            setPomodoro();
+                            setHasStarted(false);
+                        }}>Pomodoro</button>
+                        <button id='break-selector' onClick={() => {
+                            setBreak();
+                            setHasStarted(false);
+                        }}>Break</button>
+                        <button id='long-break-selector' onClick={() => {
+                            setLongBreak();
+                            setHasStarted(false);
+                        }}>Long break</button>
+                    </div>
+                    : null
+                }
 
                 <div id='time-container' style={{ 'background':timerBackground }} />
                 <ClockBorder id='time-conteiner-compleated' />
                 <p id='time'>{time}</p>
 
-                <div id='time-control-container'>
+                <div id={isFullscreenOn ? 'time-control-container-expanded' : 'time-control-container'}>
                     {
                         !hasStarted
                             ? <button onClick={() => {
@@ -79,7 +93,6 @@ const Timer = ({time, ratio, isRunning, start, pause, resume, restart, setPomodo
                                     setHasStarted(false);
                                 }} id='restart-button' >Restart</button>
                             </>
-                            
                     }
 
                 </div>
