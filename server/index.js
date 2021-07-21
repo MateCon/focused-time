6abae8ef6bcf5fb70d30c2bb6ed439018143f3d2
MateCon <68882595+MateCon@users.noreply.github.com>
2021-07-21 19:06:36 -0300

    connect heroku database

--- server/index.js
+++ server/index.js
@@ -70,5 +70,5 @@ app.get("/getStats", (req, res) => {
 });
 
 app.listen(process.env.PORT || PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${process.env.PORT || PORT}`);
 });
--- server/settings/database.js
+++ server/settings/database.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "password",
-    database: "pomodoro"
+    host: "us-cdbr-east-04.cleardb.com",
+    user: "ba3c525356c0f3",
+    password: "c0945f46",
+    database: "heroku_f39a3e2c32973da"
 });
-
+// mysql://ba3c525356c0f3:c0945f46@us-cdbr-east-04.cleardb.com/heroku_f39a3e2c32973da?reconnect=true
 module.exports = db;
