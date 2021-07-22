5901c6b2c190a5fbefea09293b02ce68136682af
MateCon <68882595+MateCon@users.noreply.github.com>
2021-07-21 21:16:17 -0300

    FIX CORS

--- client/src/pages/Stats.js
+++ client/src/pages/Stats.js
@@ -19,7 +19,7 @@ const Stats = ({user}) => {
     }
 
     const getStatsFromDB = () => {
-        Axios.get("https://focused-time.herokuapp.com/getStats", {
+        Axios.get("/getStats", {
           params: { email: user.email }
         }).then(response => {
             const data = response.data[0][0];
